2b3eb3fc6cc2e7c29254e2db0192d245b088428b
IsuruJayamanne <48113477+IsuruJayamanne@users.noreply.github.com>
2020-08-07 03:18:37 +0530

    New item UI

--- lister/src/screens/New/New.js
+++ lister/src/screens/New/New.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
+import auth from '@react-native-firebase/auth'
+import { Button } from 'galio-framework';
 
 const styles = StyleSheet.create({
     container: {
@@ -7,15 +9,72 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'palegoldenrod',
         alignItems: 'center',
+    },
+    card: {
+        width: '90%',
+        height: 400,
+        backgroundColor: 'palegoldenrod',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    row:  {
+        flexDirection: "row",
+        marginTop: 15,
+        marginBottom: 10
     }
 });
 
-const New = () => {
-    return (
-        <View style={styles.container}>
-            <Text>New</Text>
-        </View>
-    )
+export default class Settings extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+          item : "",
+          description: "",
+          
+        }
+        //this.handleSignOut = this.handleSignOut.bind(this);
+      }
+
+
+    updateInputVal = (val, prop) => {
+        console.log(prop+" "+val);
+        const state = this.state;
+        state[prop] = val;
+        this.setState(state);
+    }
+
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>New</Text>
+                <View style={styles.card}>
+                    <View style={styles.row}>
+                        <Text style={{fontSize: 25}}>            Item: </Text>
+                        <TextInput
+                            style={{ height: 40, fontSize: 20, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                            onChangeText={(val) => this.updateInputVal(val, 'item')}
+                            value={this.state.item}
+                        />
+                    </View>
+                    <View style={styles.row}>
+                        <Text style={{fontSize: 25}}>Description: </Text>
+                        <TextInput
+                            style={{ height: 80, fontSize: 20, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                            onChangeText={(val) => this.updateInputVal(val, 'description')}
+                            value={this.state.description}
+                        />
+                    </View>
+                    <View style={styles.row}>
+                        <Button
+                            color="#ff7f50"
+                            onPress={() => this.handleSignOut()}
+                        >
+                            Add
+                        </Button>
+                    </View>
+                </View>
+            </View>
+        )
+    }
 }
 
-export default New
