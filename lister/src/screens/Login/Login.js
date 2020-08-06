f3421892fc0de1022b5bab9e5028fc8b48901973
IsuruJayamanne <48113477+IsuruJayamanne@users.noreply.github.com>
2020-08-07 00:44:05 +0530

    Login auth

--- lister/src/screens/Login/Login.js
+++ lister/src/screens/Login/Login.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { View, StyleSheet, TextInput, Dimensions, KeyboardAvoidingView, StatusBar } from 'react-native'
 import { Block, Button, Text, theme, Input } from 'galio-framework'
+import auth from '@react-native-firebase/auth'
 
 const { height, width } = Dimensions.get('screen');
 const styles = StyleSheet.create({
@@ -33,66 +34,144 @@ const styles = StyleSheet.create({
 
 
 
-const Login = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Login</Text>
-            <KeyboardAvoidingView behavior="padding" enabled style={styles.container}>
-        <StatusBar barStyle="light-content" />
+export default class Login extends React.Component {
 
-          <Block row flex space="around" style={{ zIndex:2 }}>
-          <Block flex center>            
-              <Block>
-                <Text color="Black" size={60}>LOGIN</Text>
-              </Block>
-              <Block>
-                <Input placeholder="Email Address"  placeholderTextColor="black" color="black" style={styles.input}
+    constructor(props){
+        super(props);
+        this.state={
+          mobile_number : "",
+          email : "",
+          username : "",
+          password : "",
+          error_mobile_number : "",
+          error_password : "",
+          errorMessage: null
+    
+        }
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleSignUp = this.handleSignUp.bind(this);
+      }
 
-                    // onChangeText={(val) => this.updateInputVal(val, 'email')}
-                    // value={this.state.email}
-//                  onBlur = {()=>this.empty_mobile_number_validator()}
-                />
-                {/* <Text style={{color : 'red',marginLeft:20}}>{this.state.error_mobile_number}</Text> */}
-                <Input placeholder= "Password" password viewPass placeholderTextColor="black" color="black" iconColor="black" style={styles.input}
+    updateInputVal = (val, prop) => {
+        console.log(prop+" "+val);
+        const state = this.state;
+        state[prop] = val;
+        this.setState(state);
+    }
 
-                    //  onChangeText={(val) => this.updateInputVal(val, 'password')}
-                    //  value={this.state.password}
-                  //onBlur = {()=>this.empty_password_validator()}
-                />
-                {/* <Text style={{color : 'red',marginLeft:20}}>{this.state.error_password}</Text> */}
-              </Block>
-                {/* <Text style={{color : 'red',marginLeft:20}}>{this.state.errorMessage}</Text> */}
+    handleLogin() {
+        console.log("test2");
+        const { email, password } = this.state;
+        if (email != ""){
+            if (password != ""){
+                auth()
+                    .signInWithEmailAndPassword(email.toString(), password.toString())
+                    .then(() => {
+                        console.log('signed in!');
+                    })
+                    .catch(error => {
+                        if (error.code === 'auth/email-already-in-use') {
+                            //console.log('That email address is already in use!');
+                            this.setState({ errorMessage: 'That email address is already in use!' });
+                        }
+
+                        if (error.code === 'auth/invalid-email') {
+                            this.setState({ errorMessage: 'That email address is invalid!' });
+                        
+                        }
+                        console.error(error);
+                    });
+            }
+        }
+    }
+
+    handleSignUp() {
+        console.log("test1");
+        const { email, password } = this.state;
+        if (email != ""){
+            if (password != ""){
+                console.log("test3");
+                auth()
+                    .createUserWithEmailAndPassword(email, password)
+                    .then(() => {
+                        console.log('User account created & signed in!');
+                    })
+                    .catch(error => {
+                        if (error.code === 'auth/email-already-in-use') {
+                            //console.log('That email address is already in use!');
+                            this.setState({ errorMessage: 'That email address is already in use!' });
+                        }
+
+                        if (error.code === 'auth/invalid-email') {
+                            this.setState({ errorMessage: 'That email address is invalid!' });
+                        
+                        }
+                        console.error(error);
+                });
+        }   }
+    }
+
+    render() {
+        //const { navigation } = this.props;
+
+        return (
+            <View style={styles.container}>
+                <Text>Login</Text>
+                <KeyboardAvoidingView behavior="padding" enabled style={styles.container}>
+            <StatusBar barStyle="light-content" />
+
+            <Block row flex space="around" style={{ zIndex:2 }}>
+            <Block flex center>            
+                <Block>
+                    <Text color="Black" size={60}>LOGIN</Text>
+                </Block>
+                <Block>
+                    <Input placeholder="Email Address"  placeholderTextColor="black" color="black" style={styles.input}
+
+                         onChangeText={(val) => this.updateInputVal(val, 'email')}
+                         value={this.state.email}
+    //                  onBlur = {()=>this.empty_mobile_number_validator()}
+                    />
+                    {/* <Text style={{color : 'red',marginLeft:20}}>{this.state.error_mobile_number}</Text> */}
+                    <Input placeholder= "Password" password viewPass placeholderTextColor="black" color="black" iconColor="black" style={styles.input}
+                          onChangeText={(val) => this.updateInputVal(val, 'password')}
+                          value={this.state.password}
+                    //onBlur = {()=>this.empty_password_validator()}
+                    />
+                    {/* <Text style={{color : 'red',marginLeft:20}}>{this.state.error_password}</Text> */}
+                </Block>
+                    <Text style={{color : 'red',marginLeft:20}}>{this.state.errorMessage}</Text>
+                
+                <Button
+                    shadowless
+                    style={styles.button}
+                    color={'orange'}
+                    onPress={() => this.handleLogin()}
+                    >
+                    Login
+                </Button>
+                <Button
+                    shadowless
+                    style={styles.button}
+                    color={'yellowgreen'}
+                    onPress={() => this.handleSignUp()}
+                    >
+                    Sign Up
+                </Button>
+                {/* <Block row>
+                <Text color="black"> Do not have an account? </Text>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('Signup')}>
+                <Text color={materialTheme.COLORS.GREEN} style={{fontWeight:"bold"}}>Signup</Text> 
+                </TouchableOpacity>
+                </Block> */}
+                
+            </Block>
+            </Block>
+            </KeyboardAvoidingView>
             
-              <Button
-                shadowless
-                style={styles.button}
-                color={'orange'}
-                //onPress={() => this.handleLogin()}
-                >
-                Login
-              </Button>
-              <Button
-                shadowless
-                style={styles.button}
-                color={'palegreen'}
-                //onPress={() => this.handleLogin()}
-                >
-                Sign Up
-              </Button>
-              {/* <Block row>
-              <Text color="black"> Do not have an account? </Text>
-              <TouchableOpacity
-                onPress={() => navigation.navigate('Signup')}>
-               <Text color={materialTheme.COLORS.GREEN} style={{fontWeight:"bold"}}>Signup</Text> 
-              </TouchableOpacity>
-              </Block> */}
-            
-          </Block>
-          </Block>
-        </KeyboardAvoidingView>
-        
-        </View>
-    )
+            </View>
+        );
+    }
 }
 
-export default Login
